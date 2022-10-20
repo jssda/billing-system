a821d0ec206f7fc59f3fb7cd7d6c27e28918e0fa
jssd <jssdjing@gmail.com>
2022-10-20 22:50:32 +0800

    feat(init): 增加eureka客户端demo

---
+++ eureka-client-demo/src/main/java/org/team/cuc/billingsystem/eurekaclientdemo/EurekaClientDemoApplication.java
@@ -0,0 +1,16 @@
+package org.team.cuc.billingsystem.eurekaclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ */
+@SpringBootApplication
+public class EurekaClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientDemoApplication.class, args);
+    }
+
+}
--- eureka-client-demo/src/test/java/org/team/cuc/billingsystem/eurekaclientdemo/EurekaClientDemoApplicationTests.java
+++ eureka-client-demo/src/test/java/org/team/cuc/billingsystem/eurekaclientdemo/EurekaClientDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package org.team.cuc.billingsystem.eurekaserver;
+package org.team.cuc.billingsystem.eurekaclientdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class EurekaClientDemoApplicationTests {
 
     @Test
     void contextLoads() {
